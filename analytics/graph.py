15943b3d0d2f96903f5fde505cd189109b983d13
decoejz <decoejz@gmail.com>
2025-04-11 15:18:47 -0300

    wip

--- analytics/graph.py
+++ analytics/graph.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 markers = {"NO_SIGN": "o", "RSA": "s", "ECDSA": "^", "EDDSA": "D"}
 
@@ -49,7 +50,61 @@ def graph_generator(data):
             # Customize the plot
             plt.title(f"Time performance - {app} - {op}")
             plt.xlabel("Message Size (bytes)")
-            plt.ylabel("Time (ms)")
+            plt.ylabel("Time (μs)")
+            plt.legend()
+            plt.grid(True)
+
+    plt.show()
+
+
+def normalized_graph_generator(data):
+    """
+    Generate normalized graphs for the given data.
+    Parameters:
+    data (dict): A dictionary containing the split data for Autopilot and GroundControl.
+    The structure of the dictionary is as follows:
+    {
+        'Autopilot': {
+            'sign': DataFrame,
+            'verify': DataFrame
+        },
+        'GroundControl': {
+            'sign': DataFrame,
+            'verify': DataFrame
+        }
+    }
+    Returns:
+    None
+    """
+    for app in data.keys():
+        for op in data[app].keys():
+            operation = data[app][op]
+
+            operation["normalized"] = (operation["time"] - operation["time"].min()) / (
+                operation["time"].max() - operation["time"].min()
+            )
+
+            # Prepare the data for plotting
+            algorithms = ["NO_SIGN", "RSA", "ECDSA", "EDDSA"]
+            plt.figure(figsize=(10, 6))
+
+            for alg in algorithms:
+                alg_data = operation[operation["algorithm"] == alg]
+
+                if not alg_data.empty:
+                    plt.plot(
+                        alg_data["len"],
+                        alg_data["normalized"],
+                        marker=markers[alg],
+                        color=colors[alg],
+                        label=alg,
+                        linestyle="",
+                    )
+
+            # Customize the plot
+            plt.title(f"Normalized Time Performance - {app} - {op}")
+            plt.xlabel("Message Size (bytes)")
+            plt.ylabel("Normalized Time")
             plt.legend()
             plt.grid(True)
 
@@ -70,7 +125,8 @@ def boxplot_generator(data):
             operation = data[app][op]
 
             if not operation.empty:
-                operation.boxplot(column="time", by="algorithm", grid=True)
+                # operation.boxplot(column="time", by="algorithm", grid=True)
+                sns.violinplot(x="time", y="algorithm", data=operation)
 
                 # ground_control_verify = data["GroundControl"]["verify"]
 
@@ -80,7 +136,7 @@ def boxplot_generator(data):
                 plt.title(f"Boxplot of Time - {app} - {op}")
                 plt.suptitle("")  # Remove the default Pandas title
                 plt.xlabel("Algorithm")
-                plt.ylabel("Time (ms)")
+                plt.ylabel("Time (μs)")
                 plt.grid(True)
 
     plt.show()
--- analytics/main.py
+++ analytics/main.py
@@ -1,12 +1,17 @@
 from raw import process_raw_data
 from split_data import split_data
-from graph import graph_generator, boxplot_generator
+from graph import graph_generator, boxplot_generator, normalized_graph_generator
 from matrix import correlation_analysis
 
-csv_file_path = "data/teste.csv"
+# csv_file_path = "data/teste.csv"
+# csv_file_path = "data/qgc_s1_v1.csv"
+# csv_file_path = "data/px4_s1_v1.csv"
+csv_file_path = "data/s1_v1.csv"
+
 raw_data = process_raw_data(csv_file_path)
 splitted_data = split_data(raw_data)
 
 graph_generator(splitted_data)
+# normalized_graph_generator(splitted_data)
 boxplot_generator(splitted_data)
-correlation_analysis(splitted_data)
+# correlation_analysis(splitted_data)
