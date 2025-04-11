7f4972eacb278921a94a66835d876b42c1b68e98
decoejz <decoejz@gmail.com>
2025-04-11 15:20:58 -0300

    wip

--- analytics/graph.py
+++ analytics/graph.py
@@ -57,60 +57,6 @@ def graph_generator(data):
     plt.show()
 
 
-def normalized_graph_generator(data):
-    """
-    Generate normalized graphs for the given data.
-    Parameters:
-    data (dict): A dictionary containing the split data for Autopilot and GroundControl.
-    The structure of the dictionary is as follows:
-    {
-        'Autopilot': {
-            'sign': DataFrame,
-            'verify': DataFrame
-        },
-        'GroundControl': {
-            'sign': DataFrame,
-            'verify': DataFrame
-        }
-    }
-    Returns:
-    None
-    """
-    for app in data.keys():
-        for op in data[app].keys():
-            operation = data[app][op]
-
-            operation["normalized"] = (operation["time"] - operation["time"].min()) / (
-                operation["time"].max() - operation["time"].min()
-            )
-
-            # Prepare the data for plotting
-            algorithms = ["NO_SIGN", "RSA", "ECDSA", "EDDSA"]
-            plt.figure(figsize=(10, 6))
-
-            for alg in algorithms:
-                alg_data = operation[operation["algorithm"] == alg]
-
-                if not alg_data.empty:
-                    plt.plot(
-                        alg_data["len"],
-                        alg_data["normalized"],
-                        marker=markers[alg],
-                        color=colors[alg],
-                        label=alg,
-                        linestyle="",
-                    )
-
-            # Customize the plot
-            plt.title(f"Normalized Time Performance - {app} - {op}")
-            plt.xlabel("Message Size (bytes)")
-            plt.ylabel("Normalized Time")
-            plt.legend()
-            plt.grid(True)
-
-    plt.show()
-
-
 def boxplot_generator(data):
     # ! WIP
     """
